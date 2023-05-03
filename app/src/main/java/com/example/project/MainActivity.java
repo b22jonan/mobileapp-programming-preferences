cb98d85aab55d1642780895ffcdbb39c7cb3c62b
Jonathan Antfolk <61019188+walsand987@users.noreply.github.com>
2023-05-03 09:53:05 +0200

    mainactivity can read prefrences

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,12 +8,28 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    // in My MainActivity class
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
+        // In onCreate()
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
 
+
+    }
+    /*
+    // Read a preference
+    TextView prefTextRef=new TextView(this);
+    prefTextRef=(TextView)findViewById(R.id.prefText);
+    prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+*/
 }
+
+
